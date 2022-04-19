ef1e03668be6a0a85204c829b7adbecfdd827555
Yan Gonçalves <fgyan94@gmail.com>
2022-04-19 15:05:05 -0300

    :construction: Criando Goals/EvaluationGoal/Kpi/EvaluationGoalKpi

--- src/graphql/queries/EvaluationModel/index.ts
+++ src/graphql/queries/EvaluationModel/index.ts
@@ -14,4 +14,81 @@ export const GET_ALL_EVALUATION_MODEL = gql`
   }
 `
 
+export const GET_EVALUATION_MODEL = gql`
+  query GetEvaluationModel($year: String, $locale: I18NLocaleCode) {
+    evaluationModels(
+      filters: { year: { eq: $year } }
+      locale: $locale
+      sort: ["sections.order_list"]
+    ) {
+      data {
+        id
+        attributes {
+          year
+          period
+          finished
+          sections {
+            data {
+              id
+              attributes {
+                title
+                visibility
+                order_list
+                skills {
+                  data {
+                    id
+                    attributes {
+                      title
+                      description
+                    }
+                  }
+                }
+              }
+            }
+          }
+          goals {
+            data {
+              id
+              attributes {
+                goal {
+                  data {
+                    attributes {
+                      name
+                    }
+                  }
+                }
+                evaluation_goal_kpis {
+                  data {
+                    id
+                    attributes {
+                      kpi {
+                        data {
+                          attributes {
+                            name
+                          }
+                        }
+                      }
+                      target
+                      weight
+                    }
+                  }
+                }
+              }
+            }
+          }
+          feedbacks {
+            data {
+              id
+              attributes {
+                order_list
+                question
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 export * from './__generated__/GetAllEvaluationModel'
--- src/graphql/queries/EvaluationModel/types.ts
+++ src/graphql/queries/EvaluationModel/types.ts
@@ -9,3 +9,48 @@ export const GET_ALL_EVALUATION_MODEL = gql`
     }
   }
 `
+
+export const GET_EVALUATION_MODEL = gql`
+  query GetEvaluationModel($year: String, $locale: I18NLocaleCode) {
+    evaluationModels(
+      filters: { year: { eq: $year } }
+      locale: $locale
+      sort: ["sections.order_list"]
+    ) {
+      id
+      year
+      period
+      finished
+      sections {
+        id
+        title
+        visibility
+        order_list
+        skills {
+          id
+          title
+          description
+        }
+      }
+      goals {
+        id
+        goal {
+          name
+        }
+        evaluation_goal_kpis {
+          id
+          kpi {
+            name
+          }
+          target
+          weight
+        }
+      }
+      feedbacks {
+        id
+        order_list
+        question
+      }
+    }
+  }
+`
