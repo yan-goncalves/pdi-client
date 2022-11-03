9ec1b4de47332fb4392d1f893fc63775da78952e
Yan Gonçalves <fgyan94@gmail.com>
2022-11-02 23:06:08 -0300

    feat: get evaluation and usersGoals according to year param in report route

--- src/pages/report/[year]/index.tsx
+++ src/pages/report/[year]/index.tsx
@@ -1,16 +1,35 @@
 import { ROLES } from 'constants/role'
+import { useEvaluation } from 'contexts/EvaluationProvider'
 import { initializeApollo } from 'graphql/client'
 import { GetDepartments } from 'graphql/queries/collection/Department'
+import { GET_EVALUATION_MODEL } from 'graphql/queries/collection/EvaluationModel'
+import { GET_EVALUATION_GOALS } from 'graphql/queries/collection/Goals'
 import { GET_USERS } from 'graphql/queries/collection/User'
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
+import { useEffect } from 'react'
 import ReportUserListTemplate, { ReportUserListProps } from 'templates/Report/ReportUserList'
 import { GetDepartmentsType } from 'types/collection/Department'
+import { EvaluationModelType, GetEvaluationModelType } from 'types/collection/EvaluationModel'
+import { GetEvaluationGoalsType } from 'types/collection/Goal'
 import { GetUsersType } from 'types/collection/User'
 import { orderMembersByDepartments } from 'utils/helpers'
 
-const ReportYear = ({ users, year }: ReportUserListProps) => {
-  return <ReportUserListTemplate users={users} year={year} />
+const ReportYear = ({
+  users,
+  year,
+  usersGoals,
+  evaluationModel
+}: ReportUserListProps & { evaluationModel: EvaluationModelType }) => {
+  const { setEvaluationModel } = useEvaluation()
+
+  useEffect(() => {
+    if (evaluationModel) {
+      setEvaluationModel(evaluationModel)
+    }
+  }, [evaluationModel])
+
+  return <ReportUserListTemplate users={users} year={year} usersGoals={usersGoals} />
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, locale, params }) => {
@@ -24,6 +43,28 @@ export const getServerSideProps: GetServerSideProps = async ({ req, locale, para
   })
 
   if (!users) {
+    return {
+      notFound: true
+    }
+  }
+
+  const {
+    data: { evaluation },
+    error: evaluationError,
+    errors: evaluationErrors
+  } = await apolloClient.query<GetEvaluationModelType>({
+    query: GET_EVALUATION_MODEL,
+    variables: {
+      year: Number(params?.year)
+    },
+    context: {
+      headers: {
+        locale
+      }
+    }
+  })
+
+  if (!evaluation || evaluationError || evaluationErrors) {
     return {
       notFound: true
     }
@@ -50,10 +91,35 @@ export const getServerSideProps: GetServerSideProps = async ({ req, locale, para
   // const orderedTeams =
   orderMembersByDepartments(team, departments)
 
+  const { usersGoals }: Pick<ReportUserListProps, 'usersGoals'> = { usersGoals: {} }
+  for (const user of team) {
+    const {
+      data: { evaluationGoals }
+    } = await apolloClient.query<GetEvaluationGoalsType>({
+      query: GET_EVALUATION_GOALS,
+      variables: {
+        idEvaluation: evaluation.id,
+        idUser: user.id
+      }
+    })
+
+    if (!evaluationGoals) {
+      return {
+        notFound: true
+      }
+    }
+
+    if (!Array.isArray(usersGoals[user.id])) usersGoals[user.id] = []
+
+    usersGoals[user.id].push(...evaluationGoals)
+  }
+
   return {
     props: {
       users: team,
-      year: params?.year
+      year: params?.year,
+      evaluationModel: evaluation,
+      usersGoals
     }
   }
 }
