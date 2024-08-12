b626caad7fa23b1e37e511878084b6957700b9f2
Yan Goncalves <fgyan94@gmail.com>
2024-08-12 17:05:14 -0300

    feat(goals): notify manager when goals are not completed configured

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from '@apollo/client'
 import { Button, Space, Text, useMantineTheme } from '@mantine/core'
 import { useNotifications } from '@mantine/notifications'
 import { Grid, Input } from '@nextui-org/react'
@@ -7,10 +8,16 @@ import { CommonConstants } from 'constants/common'
 import { ErrorsConstants } from 'constants/errors'
 import { ROLES } from 'constants/role'
 import { useLocale } from 'contexts/LocaleProvider'
+import { GET_EVALUATION_MODEL } from 'graphql/queries/collection/EvaluationModel'
+import { GET_EVALUATION_GOALS } from 'graphql/queries/collection/Goals'
+import { GET_TEAM_MEMBERS } from 'graphql/queries/collection/Team'
 import { getSession, signIn } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { GetEvaluationModelType } from 'types/collection/EvaluationModel'
+import { GetEvaluationGoalsType } from 'types/collection/Goal'
+import { GetTeamMembersType } from 'types/collection/Team'
 import { ButtonApiProps } from 'types/common'
 import { useStyles } from './styles'
 
@@ -40,6 +47,14 @@ const LoginForm = ({ usernameLabel, passwordLabel, button }: LoginFormProps) => 
     clearErrors,
     watch
   } = useForm<Inputs>()
+
+  const { refetch: fetchEvaluationModel } = useQuery<GetEvaluationModelType>(GET_EVALUATION_MODEL, {
+    variables: {
+      year: new Date().getFullYear()
+    }
+  })
+  const { refetch: fetchTeamMembers } = useQuery<GetTeamMembersType>(GET_TEAM_MEMBERS)
+  const { refetch: fetchTeamGoals } = useQuery<GetEvaluationGoalsType>(GET_EVALUATION_GOALS)
 
   const username = watch('username')
   const password = watch('password')
@@ -88,32 +103,97 @@ const LoginForm = ({ usernameLabel, passwordLabel, button }: LoginFormProps) => 
           }
         })
       } else if (res?.url) {
-        await push(res.url).then(async () => {
-          const session = await getSession()
-          const user = session?.user
-          const info = user?.info
-          const name = user?.role === ROLES.ADMIN ? 'Admin' : info?.name
+        await push(res.url)
+          .then(async () => {
+            const session = await getSession()
+            const user = session?.user
+            const info = user?.info
+            const name = user?.role === ROLES.ADMIN ? 'Admin' : info?.name
 
-          notifications.showNotification({
-            message: (
-              <Text
-                style={{ padding: 2 }}
-                dangerouslySetInnerHTML={{
-                  __html: `${CommonConstants.welcome[locale](name)} 😎`
-                }}
-              />
-            ),
-            color: 'green',
-            radius: 'md',
-            autoClose: 3000,
-            styles: (theme) => ({
-              root: {
-                borderColor: theme.colors.green[6],
-                '&::before': { backgroundColor: theme.colors.green[6] }
-              }
+            notifications.showNotification({
+              message: (
+                <Text
+                  style={{ padding: 2 }}
+                  dangerouslySetInnerHTML={{
+                    __html: `${CommonConstants.welcome[locale](name)} 😎`
+                  }}
+                />
+              ),
+              color: 'green',
+              radius: 'md',
+              autoClose: 3000,
+              styles: (theme) => ({
+                root: {
+                  borderColor: theme.colors.green[6],
+                  '&::before': { backgroundColor: theme.colors.green[6] }
+                }
+              })
             })
           })
-        })
+          .finally(async () => {
+            const session = await getSession()
+            const user = session?.user
+            const info = user?.info
+            const name = user?.role === ROLES.ADMIN ? 'Admin' : info?.name
+
+            if (user?.role !== ROLES.USER) {
+              const { data: dataEvaluationModel } = await fetchEvaluationModel()
+              const { data: dataTeamMembers } = await fetchTeamMembers()
+              let missingTotalWeight = false
+
+              if (dataEvaluationModel?.evaluation && dataTeamMembers?.team) {
+                const evaluation = dataEvaluationModel.evaluation
+                const team = dataTeamMembers.team
+
+                for (const user of team) {
+                  const { data: dataTeamGoals } = await fetchTeamGoals({
+                    idEvaluation: evaluation.id,
+                    idUser: user.id
+                  })
+                  const total = dataTeamGoals?.evaluationGoals
+                    .map(({ kpis }) => {
+                      return kpis
+                        .map(({ weight }) => weight)
+                        .reduce((prevWeight, currWeight) => prevWeight + currWeight, 0)
+                    })
+                    .reduce((prevWeight, currWeight) => prevWeight + currWeight, 0)
+
+                  if (total < 100) {
+                    missingTotalWeight = true
+                    break
+                  }
+                }
+
+                if (missingTotalWeight) {
+                  notifications.showNotification({
+                    message: (
+                      <Text
+                        style={{ padding: 2 }}
+                        dangerouslySetInnerHTML={{
+                          __html: `<strong>${name}</strong>, há membros do time que não estão com os objetivos totalmente configurados para a avalição atual. <br/> <i>Ir para <a href="/manager/goals">Objetivos do Time</a>.</i>`
+                        }}
+                      />
+                    ),
+                    color: 'red',
+                    radius: 'md',
+                    autoClose: false,
+                    styles: (theme) => ({
+                      root: {
+                        backgroundColor: theme.colors.red[1],
+                        borderColor: theme.colors.red[1],
+
+                        '&::before': { backgroundColor: theme.colors.red[9] }
+                      },
+                      closeButton: {
+                        color: theme.colors.red[7],
+                        '&:hover': { backgroundColor: theme.colors.red[2] }
+                      }
+                    })
+                  })
+                }
+              }
+            }
+          })
       }
     })
   }
