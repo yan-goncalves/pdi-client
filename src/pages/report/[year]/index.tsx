bc133d4752659b335c2fd304383e4c047cc6c7a1
Yan Goncalves <fgyan94@gmail.com>
2025-03-20 09:46:16 -0300

    refactor(reports): send only important data to backend on generating report

--- src/pages/report/[year]/index.tsx
+++ src/pages/report/[year]/index.tsx
@@ -2,20 +2,17 @@ import { ROLES } from 'constants/role'
 import { useEvaluation } from 'contexts/EvaluationProvider'
 import { initializeApollo } from 'graphql/client'
 import { GET_EVALUATION_MODEL } from 'graphql/queries/collection/EvaluationModel'
-import { GET_EVALUATION_GOALS } from 'graphql/queries/collection/Goals'
 import { GET_USERS } from 'graphql/queries/collection/User'
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/react'
 import { useEffect } from 'react'
 import ReportUserListTemplate, { ReportUserListProps } from 'templates/Report/ReportUserList'
 import { EvaluationModelType, GetEvaluationModelType } from 'types/collection/EvaluationModel'
-import { GetEvaluationGoalsType } from 'types/collection/Goal'
 import { GetUsersType } from 'types/collection/User'
 
 const ReportYear = ({
   users,
   year,
-  usersGoals,
   evaluationModel
 }: ReportUserListProps & { evaluationModel: EvaluationModelType }) => {
   const { setEvaluationModel } = useEvaluation()
@@ -26,7 +23,7 @@ const ReportYear = ({
     }
   }, [evaluationModel])
 
-  return <ReportUserListTemplate users={users} year={year} usersGoals={usersGoals} />
+  return <ReportUserListTemplate users={users} year={year} />
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, locale, params }) => {
@@ -78,35 +75,11 @@ export const getServerSideProps: GetServerSideProps = async ({ req, locale, para
       return userA.username > userB.username ? 1 : userA.username < userB.username ? -1 : 0
     })
 
-  const { usersGoals }: Pick<ReportUserListProps, 'usersGoals'> = { usersGoals: {} }
-  for (const user of team) {
-    const {
-      data: { evaluationGoals }
-    } = await apolloClient.query<GetEvaluationGoalsType>({
-      query: GET_EVALUATION_GOALS,
-      variables: {
-        idEvaluation: evaluation.id,
-        idUser: user.id
-      }
-    })
-
-    if (!evaluationGoals) {
-      return {
-        notFound: true
-      }
-    }
-
-    if (!Array.isArray(usersGoals[user.id])) usersGoals[user.id] = []
-
-    usersGoals[user.id].push(...evaluationGoals)
-  }
-
   return {
     props: {
       users: team.filter((user) => !user.username.includes('pdi')),
       year: params?.year,
-      evaluationModel: evaluation,
-      usersGoals
+      evaluationModel: evaluation
     }
   }
 }
